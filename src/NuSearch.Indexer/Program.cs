2b510a3eea9531365cf83fdf55ba19a7b8b198f9
Alex Yang <ayang@roblox.com>
2020-05-10 20:20:09 -0700

    elasticsearch .net example fixed for ES 7.x

---
+++ src/NuSearch.Indexer/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Threading;
+using Nest;
+using NuSearch.Domain;
+using NuSearch.Domain.Data;
+using NuSearch.Domain.Model;
+
+namespace NuSearch.Indexer
+{
+	class Program
+	{
+		private static ElasticClient Client { get; set; }
+		private static NugetDumpReader DumpReader { get; set; }
+
+		static void Main(string[] args)
+		{
+			Client = NuSearchConfiguration.GetClient();
+			var directory = args.Length > 0 && !string.IsNullOrEmpty(args[0]) 
+				? args[0] 
+				: NuSearchConfiguration.PackagePath;
+			DumpReader = new NugetDumpReader(directory);
+
+			DeleteIndexIfExists();
+			IndexDumps();
+
+			Console.WriteLine("Press any key to exit.");
+			Console.ReadKey();
+		}
+
+		// Add this method.
+		static void DeleteIndexIfExists()
+		{
+			if (Client.Indices.Exists("nusearch").Exists) {
+				Console.WriteLine("Found existing nusearch index, deleting...");
+			    Client.Indices.Delete("nusearch");
+				Console.WriteLine("Index nusearch deleted.");
+            }
+		}
+
+        static void IndexDumps()
+        {
+            var packages = DumpReader.Dumps.First().NugetPackages;
+
+            Console.Write("Indexing documents into Elasticsearch...");
+
+            foreach (var package in packages)
+            {
+                var result = Client.IndexDocument(package);
+
+                if (!result.IsValid)
+                {
+                    Console.WriteLine(result.DebugInformation);
+                    Console.Read();
+                    Environment.Exit(1);
+                }
+            }
+
+            Console.WriteLine("Done.");
+        }
+    }
+}
+
---
+++ src/NuSearch.Web/Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+using NuSearch.Domain.Model;
+using NuSearch.Web.Models;
+
+namespace NuSearch.Web.Controllers
+{
+	public class SearchController : Controller
+    {
+		private readonly IElasticClient _client;
+
+		public SearchController(IElasticClient client) => _client = client;
+
+	    [HttpGet]
+        public IActionResult Index(SearchForm form)
+        {
+			// TODO: You'll write most of the implementation here
+			var model = new SearchViewModel();
+
+	        return View(model);
+		}
+    }
+}
